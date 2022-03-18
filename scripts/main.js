1c2cd932af5e8e69381a8a30259715db1f8f9771
chloe choquet <chloe.choquet@gmail.com>
2022-03-18 17:18:33 +0100

    base du projet

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 import {recipes} from './../data/recipes.js';
-import {recipeClass} from './model/recipe.js';
 
 
 
@@ -8,6 +7,7 @@ const input = document.getElementById('inputSearch');
 const errorH2 = document.querySelector('.error');
 
 
+//===========Création du DOM===============
 
 const recipesDom = (data) =>{
 
@@ -77,19 +77,18 @@ const recipesDom = (data) =>{
 
 function displayRecipe(value){
     if(value.length === 0){
-        console.log('array is empty')
-        errorH2.innerHTML = 'Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc..'
+        console.log('array is empty');
+        errorH2.innerHTML = 'Aucune recette ne correspond à votre critère... vous pouvez chercher « tarte aux pommes », « poisson », etc..';
     }else{
         errorH2.innerHTML = '',
         value.forEach(e =>{
-            recipesDom(e)
+            recipesDom(e);
         }) 
-        console.log(value)
+        console.log(value);
     }
     
     
 }
-
 
 displayRecipe(recipes);
 
@@ -100,81 +99,15 @@ input.addEventListener('input', (e) =>{
 
 if(e.target.value.length >= 3){
     sectionRecipes.innerHTML = '';
-    let newData = findByPrincipal(recipes, e.target.value);
+    let newData = findByOld(recipes, e.target.value);
     displayRecipe(newData)
 }else{
     sectionRecipes.innerHTML = '';
-    displayRecipe(recipes)
+    displayRecipe(recipes);
 }
 })
 
-//=============fonction tri avec l'input principal (filter)==============
 
-function findByPrincipal(data, requete){
-if(requete.includes(' ')){
-    console.log('ya un espace')
-}
-    return data.filter((el) =>{
-        let title = el.name;
-        let desc = el.description;
-        let ingred = el.ingredients;
-
-        let resultTitle = title.toLowerCase().indexOf(requete.toLowerCase())!== -1;
-        let resultDesc = desc.toLowerCase().indexOf(requete.toLowerCase())!== -1;
-
-        let resultIngred = ingred.map((i) => i.ingredient.toLowerCase().indexOf(requete.toLowerCase())!== -1);
-        if(resultIngred.includes(true)){
-            console.log(resultIngred, el)
-            return el
-        }  
-        
-        /* for(let i of ingred){
-            let listIngred = i.ingredient;
-            
-            const resultIngred = listIngred.toLowerCase().indexOf(requete.toLowerCase())!== -1;
-            if(resultIngred){
-                return el
-            }
-        }   */ 
-
-        return resultTitle, resultDesc ;
-    })
-}
-
-//=============fonction tri avec l'input principal (for)==============
-
-function findByOld(data, requete){
-    let arrtest = []
-    for(let i in data){
-        let arr = data[i];
-        let title = arr.name;
-        let desc = arr.description;
-        let ListIngred = arr.ingredients
-        
-        const resultTitle = title.toLowerCase().includes(requete.toLowerCase());
-        const resultDesc = desc.toLowerCase().includes(requete.toLowerCase());
-
-        for(let j in ListIngred){
-            let ingred = ListIngred[j].ingredient;
-            
-            const resultIngred = ingred.toLowerCase().includes(requete.toLowerCase());
-            if(resultIngred){
-                arrtest.push(data[i]);
-                console.log(data[i]);
-            } 
-        }
-
-        if(resultTitle){
-            arrtest.push(data[i]);
-            console.log(title,arrtest);
-
-        }else if(resultDesc){
-            arrtest.push(data[i]);
-        }
-    }
-    console.log(arrtest);
-    return arrtest;
-}
 
 
 
